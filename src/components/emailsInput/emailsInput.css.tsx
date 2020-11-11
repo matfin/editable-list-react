f86b4ecf755f3624d5ce616d0dd06fa76ca323dd
Matt Finucane <matfin@users.noreply.github.com>
2020-11-11 16:55:47 +0000

    feat: make container width flexible and add scroll to editor area (#8)

--- src/components/emailsInput/emailsInput.css.tsx
+++ src/components/emailsInput/emailsInput.css.tsx
@@ -3,9 +3,11 @@ import InputBocComponent from 'components/inputbox';
 
 export const Container = styled.ul`
   display: block;
+  max-height: 6rem;
   background: #fff;
   border: 1px solid rgb(195, 194, 207);
   border-radius: 0.25rem;
+  overflow-y: scroll;
 `;
 
 export const BoxItem = styled.li`
