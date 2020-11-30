9e5bfe7f47961b626be0cdfa2e3cb678e2dc13c3
Matt Finucane <matfin@users.noreply.github.com>
2020-11-30 08:57:24 +0000

    feat: show count for emails being added (#13)

--- dist/example.js
+++ dist/example.js
@@ -19,41 +19,46 @@ window.addEventListener('load', async () => {
   const optimisedEl = document.querySelector('#optimised .emails-input');
   const optimisedAddButton = document.querySelector('#optimised .add-email');
   const optimisedCountButton = document.querySelector('#optimised .get-count');
+  const optimisedIndicator = document.querySelector('#optimised .indicator');
   const optimised = await emailBoard.init(optimisedEl, true);
+
 
   // initialise the control component
   const controlEl = document.querySelector('#control .emails-input');
   const controlAddButton = document.querySelector('#control .add-email');
   const controlCountButton = document.querySelector('#control .get-count');
+  const controlIndicator = document.querySelector('#control .indicator');
   const control = await emailBoard.init(controlEl, false);
 
   // add a large number of emails to the optimised list
   optimisedAddButton.addEventListener('click', () => {
-    for (let i = 0; i < 400; i++) {
+    for (let i = 0; i < 200; i++) {
       setTimeout(() => {
         const index = randomNumberInRange(0, randomEmails.length);
 
         optimised.addEmailAddress(randomEmails[index]);
-      }, 5);
+        optimisedIndicator.innerHTML = `Adding: ${i + 1} items`;
+      });
     }
   });
   // get the count
   optimisedCountButton.addEventListener('click', () =>
-    alert(optimised.getEmailCount())
+    optimisedIndicator.innerHTML = `Total items: ${optimised.getEmailCount()}`
   );
 
   // add a large number of emails to the control list
   controlAddButton.addEventListener('click', () => {
-    for (let i = 0; i < 400; i++) {
+    for (let i = 0; i < 200; i++) {
       setTimeout(() => {
         const index = randomNumberInRange(0, randomEmails.length);
 
         control.addEmailAddress(randomEmails[index]);
-      }, 5);
+        controlIndicator.innerHTML = `Adding: ${i + 1} items`;
+      });
     }
   });
   // get the count
   controlCountButton.addEventListener('click', () =>
-    alert(control.getEmailCount())
+    controlIndicator.innerHTML = `Total items: ${control.getEmailCount()}`
   );
 });
